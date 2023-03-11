12443611020cbbc8052d09f4f1a88546658bc6a5
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2023-03-12 04:18:15 +0530

    Update image_resizer.ps1

--- image_resizer.ps1
+++ image_resizer.ps1
@@ -1,11 +1,12 @@
 $rating_int = [int]($args[0])
 $n = [int]($args[1])
-$step = [int]($args[2])
+# $step = [int]($args[2])
 $start = [int](Get-Content -Path ("category_" + $rating_int + "_formatted/category_" + $rating_int + "_formatted_image_count.txt"))
+$img_cnt = [int](Get-Content -Path ("category_" + $rating_int + "/category_" + $rating_int + "_image_count.txt"))
+$step = [System.Math]::Ceiling(($img_cnt - $start) / $n)
 
 for ($i = 0; $i -lt $n; $i++) {
     .\image_resizer.py $rating_int ($start + $i * $step) ($start + ($i + 1) * $step)
 }
 
-$img_cnt = [int](Get-Content -Path ("category_" + $rating_int + "/category_" + $rating_int + "_image_count.txt"))
 Write-Output ([System.Math]::Min($img_cnt, $start + $n * $step)) > ("category_" + $rating_int + "_formatted/category_" + $rating_int + "_formatted_image_count.txt")
